3244aeede84854079bbd026bc2a6b8ddfc2a7062
kavin-uzumaki <132762076+kavin-uzumaki@users.noreply.github.com>
2025-06-23 12:37:23 +0530

    23-06-2023 auto downloads files test 003

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
+
+echo "Installing system packages..."
+apt-get update && apt-get install -y chromium
+
 echo "Installing Python dependencies..."
 pip install -r requirements.txt
+
