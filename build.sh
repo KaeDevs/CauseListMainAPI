605e66ec1edd732fb608752dc198b692bc655891
kavin-uzumaki <132762076+kavin-uzumaki@users.noreply.github.com>
2025-06-23 16:12:38 +0530

    23-06-2023 auto downloads files test 008

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
+
+
 #!/usr/bin/env bash
-
-echo "Installing system packages..."
 apt-get update
-apt-get install -y chromium-driver chromium
-
-echo "Installing Python dependencies..."
-pip install -r requirements.txt
+apt-get install -y chromium-browser chromium-chromedriver
+which chromedriver
 
 
